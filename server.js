b61b51298c45cdf89626675e8e050fa14088a0a4
somaisuraj <somai.suraj1990@gmail.com>
2019-01-06 22:44:37 +0900

    setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 // hbs.register is part of html content and __dirname is first parent folder address
@@ -66,4 +67,6 @@ app.get('/bad', (req, res) => {
 
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`server is runnig at ${port} port`);
+});
